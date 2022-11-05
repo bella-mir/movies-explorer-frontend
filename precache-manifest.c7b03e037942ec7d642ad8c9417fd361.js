edc8b1f7ef30f6905dc52b3c945bd42aeb3c6fd4
Bella Mironova <bellamironova@gmail.com>
2022-11-05 18:33:50 +0300

    Updates

---
+++ precache-manifest.c7b03e037942ec7d642ad8c9417fd361.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9770dfa7313c00ba33c0",
+    "url": "/static/css/main.daa34f96.chunk.css"
+  },
+  {
+    "revision": "9770dfa7313c00ba33c0",
+    "url": "/static/js/main.dba830fe.chunk.js"
+  },
+  {
+    "revision": "7d85caf2a602f0a14c63",
+    "url": "/static/js/runtime~main.ae86a450.js"
+  },
+  {
+    "revision": "9e86fd316a4e40f3340a",
+    "url": "/static/js/2.7b42a420.chunk.js"
+  },
+  {
+    "revision": "dfef2813c51b7c1e772e",
+    "url": "/static/js/3.c015642e.chunk.js"
+  },
+  {
+    "revision": "a6e0015d36ba8db629d540b74b6712e1",
+    "url": "/static/media/arrow.a6e0015d.svg"
+  },
+  {
+    "revision": "fe299dff7a68993bc6c2d9381cb15fde",
+    "url": "/static/media/close.fe299dff.svg"
+  },
+  {
+    "revision": "053b1dc771ef51a9f36afda59ae2ebea",
+    "url": "/static/media/logo.053b1dc7.svg"
+  },
+  {
+    "revision": "2cfedefac0fff82296337334369e245d",
+    "url": "/static/media/find_icon.2cfedefa.svg"
+  },
+  {
+    "revision": "b737c69d76018e77f97d1c82ce81d6b1",
+    "url": "/static/media/globe.b737c69d.svg"
+  },
+  {
+    "revision": "b77b90cbd5e4d8480a8a03bc862d4ddf",
+    "url": "/static/media/myphoto.b77b90cb.jpeg"
+  },
+  {
+    "revision": "4d127124f941ebf54a749cf9ab10e545",
+    "url": "/static/media/Inter-Regular.4d127124.woff"
+  },
+  {
+    "revision": "0dae34d5a70aa5062a7dd26ae030780d",
+    "url": "/static/media/Inter-Bold.0dae34d5.woff2"
+  },
+  {
+    "revision": "d22a0d6612cceed0724e681a3a1e90da",
+    "url": "/static/media/Inter-Regular.d22a0d66.woff2"
+  },
+  {
+    "revision": "30690bb6519a21e77ed29ff123497df3",
+    "url": "/static/media/Inter-Bold.30690bb6.woff"
+  },
+  {
+    "revision": "af718f919aafb2f3178ac634c7f37082",
+    "url": "/static/media/Inter-Black.af718f91.woff"
+  },
+  {
+    "revision": "49c59986c2a8f1a08a4002b17e2dc62d",
+    "url": "/static/media/Inter-Black.49c59986.woff2"
+  },
+  {
+    "revision": "84e7cc776ccfdad05fea9d1727d63e9f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movies-explorer-frontend/precache-manifest.eec0f135424c35bd5e51bfe93bc26993.js"
+  "/precache-manifest.c7b03e037942ec7d642ad8c9417fd361.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/movies-explorer-frontend/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
